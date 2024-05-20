74aa7fea6f18bcc4bc1fab65bcbf9dfeeafa5094
Jacopo Guoyi Chen <jacopo.chen256@gmail.com>
2024-05-20 14:47:14 +0200

    configs docs

--- twisted_l2/configs.py
+++ twisted_l2/configs.py
@@ -1,41 +1,78 @@
 class FGARankOptions:
-    """When reducing a finite group algebra matrix, invert bicyclic units."""
+    """
+    Configuration for the computations of ranks over the algebra of a finite group.
+    
+        - BICYCLIC:
+            When reducing a finite group algebra matrix, invert bicyclic units.
+            Default: True.
+            
+        - TRANSPOSE:
+            Transpose before calculating rank.
+            Default: False.
+            
+        - CEILING:
+            Return the ceiling of the rank.
+            Default: False.
+            
+        - SPARSE:
+            Use sparse matrices.
+            Default: True.
+            
+        - RING:
+            Ring used for rank computations. Allowed values:
+
+            - "rational" : rationals (fastest, exact).
+            - "real"     : reals with 53 bits of precision, inaccurate due to rounding errors.
+            - "double"   : reals with NumPy, probably accurate but slow, at least for small matrices.
+            
+            Default: "rational".
+            
+        - TRY_REDUCE:
+            Use Gauss moves before restriction of scalars.
+            Default: False.
+    """
+    
     BICYCLIC = True
-    
-    """Transpose before calculating rank."""
     TRANSPOSE = False
-    
-    """Return the ceiling of the rank."""
     CEILING = False
-    
-    """Use sparse matrices."""
     SPARSE = True
-    
-    """
-    Ring used for rank computations. Allowed values:
-    
-        - "rational" : rationals (fastest, exact).
-        - "real"     : reals with 53 bits of precision, inaccurate due to rounding errors.
-        - "double"   : reals with NumPy, probably accurate but slow, at least for small matrices.
-    """
     RING = "rational"
-    
-    """Use Gauss moves before restriction of scalars."""
     TRY_REDUCE = False
     
 class TwistedRankOptions:
-    """Print the matrix over Q[L]."""
-    PRINT_FIN = False
+    """
+    Configuration for the von_neumann_rank and determinant_degree routines.
     
-    """Ask whether to save a copy of the expanded matrix and the matrix over Q[L]."""
+        - PRINT_FIN:
+            Print the matrix over Q[L].
+            Default: False.
+            
+        - ASK_INPUT:
+            Ask whether to save a copy of the expanded matrix and the matrix over Q[L].
+            Default: False.
+    """
+    
+    PRINT_FIN = False
     ASK_INPUT = False
 
 class LogOptions:
-    """Level of logging verbosity"""
+    """
+    Configuration for logging.
+    
+        - LEVEL:
+            Level of logging verbosity.
+            Default: SILENT.
+        
+        - PRECISION:
+            Precision of values displayed during logging (number of digits, or PREC_EXACT).
+            Default: 15.
+            
+    Constants:
+        
+        - PREC_EXACT:
+            Constant value for `PRECISION`. Print rational values as fractions.
+    """
+    
     LEVEL = 0
-    
-    """Constant value for `PRECISION`. Print rational values as fractions."""
     PREC_EXACT = -1
-    
-    """Precision of values displayed during logging (number of digits, or `PREC_EXACT`)"""
     PRECISION = 15
